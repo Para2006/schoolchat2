2002a090c0a76336db48869ccea8cfcf77656d51
Para2006 <103443216+Para2006@users.noreply.github.com>
2022-10-19 02:01:08 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAe6dkKB7VV_m89IyB4rzGbShzjoZtqemc",
+    authDomain: "newp-4166e.firebaseapp.com",
+    databaseURL: "https://newp-4166e-default-rtdb.firebaseio.com",
+    projectId: "newp-4166e",
+    storageBucket: "newp-4166e.appspot.com",
+    messagingSenderId: "422916803277",
+    appId: "1:422916803277:web:303ce8bbb7501b745cf784"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
